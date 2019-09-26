60ab2da46058e8525493763cde6eaeb4e08c66c5
wjj <wangjiajie@zju.edu.cn>
2019-09-26 13:30:23 +0800

    project init

---
+++ src/main/java/com/wjj/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.wjj.mmall.dao;
+
+import com.wjj.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
